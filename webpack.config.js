cb403068e8e440a64569750145c5c9d66325e444
alex919090 <alex919090@gmail.com>
2022-05-13 20:52:40 -0400

    Configuración de plugins y loaders para React con sus fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,42 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: 'src/index.js',
-    output: {
-        path: path.resolve( __dirname , 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js' , '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    { loader: 'html-loader,' }
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          { loader: 'html-loader' }
         ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './plublic/index.html',
-            filename: 'index.html'
-        })
-    ],
-    devServer: {
-        contentBase: path.join(__dirname,'dist'),
-        compress: true,
-        port: 4000
-    }
+    compress: true,
+    port: 4000
+}
 }
