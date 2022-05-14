046c2b889e1320c05d20ba27d8eef3df55bfd896
alex919090 <alex919090@gmail.com>
2022-05-13 21:02:50 -0400

    Configuración de Webpack para CSS en React

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import reactDom from 'react-dom';
 import ReactDom from 'react-dom';
 import App from './components/App';
+import './styles/global.scss'
 
 reactDom.render(<App />, document.getElementById('app'));
